b9d33728e1955d361b69e08d629d8ee9a08a4d1b
ArtificialLegacy <redsbro457@gmail.com>
2024-08-02 18:12:36 -0400

    add merchant data

--- data.js
+++ data.js
@@ -1,10 +1,122 @@
 
 const arrow = {
-    name: "Arrow"
+    name: "Arrow",
 }
 
 const bolt = {
-    name: "Bolt"
+    name: "Bolt",
+}
+
+const kukri = {
+    name: "Kukri",
+}
+
+const lightningBolt = {
+    name: "Lightning Bolt",
+}
+
+const greatArrow = {
+    name: "Great Arrow",
+}
+
+const explosiveBolt = {
+    name: "Explosive Bolt",
+}
+
+const ballistaBolt = {
+    name: "Ballista Bolt",
+}
+
+const explosiveGreatBolt = {
+    name: "Explosive Greatbolt",
+}
+
+const poisonboneDart = {
+    name: "Poisonbone Dart",
+}
+
+const poisonedStone = {
+    name: "Poisoned Stone"
+}
+
+const poisonedStoneClump = {
+    name: "Poisoned Stone Clump"
+}
+
+const fireArrow = {
+    name: "Fire Arrow"
+}
+
+const flamingBolt = {
+    name: "Flaming Bolt",
+}
+
+const meteorBolt = {
+    name: "Meteor Bolt",
+}
+
+const serpentArrow = {
+    name: "Serpent Arrow",
+}
+
+const goldenArrow = {
+    name: "Golden Arrow",
+}
+
+const goldenGreatArrow = {
+    name: "Golden Great Arrow",
+}
+
+const goldenBolt = {
+    name: "Golden Bolt",
+}
+
+const lightningGreatbolt = {
+    name: "Lightning Greatbolt",
+}
+
+const bloodrose = {
+    name: "Bloodrose",
+}
+
+const dwellingArrow = {
+    name: "Dwelling Arrow",
+}
+
+const burredBolt = {
+    name: "Burred Bolt",
+}
+
+const throwingDagger = {
+    name: "Throwing Dagger",
+}
+
+const glassShard = {
+    name: "Glass Shard",
+}
+
+const boiledPrawn = {
+    name: "Boiled Prawn",
+}
+
+const boiledCrab = {
+    name: "Boiled Crab",
+}
+
+const somberStone1 = {
+    name: "Somber Smithing Stone [1]",
+}
+
+const somberStone2 = {
+    name: "Somber Smithing Stone [2]",
+}
+
+const radahnSpear = {
+    name: "Radahn's Spear",
+}
+
+const festeringBloodyfinger = {
+    name: "Festering Bloody Finger"
 }
 
 const nomadicMerchant1 = {
@@ -17,14 +129,299 @@ const nomadicMerchant1 = {
     ],
 }
 
+const nomadicMerchant2 = {
+    name: "Nomadic Merchant 2",
+    location: "East Limgrave",
+    link: "https://eldenring.wiki.fextralife.com/Nomadic+Merchant+East+Limgrave",
+    stock: [
+        [arrow, 20],
+        [bolt, 40],
+    ],
+}
+
+const nomadicMerchant3 = {
+    name: "Nomadic Merchant 3",
+    location: "West Limgrave",
+    link: "https://eldenring.wiki.fextralife.com/Nomadic+Merchant+West+Limgrave",
+    stock: [
+        [arrow, 20],
+        [bolt, 40],
+    ],
+}
+
+const nomadicMerchant4 = {
+    name: "Nomadic Merchant 4",
+    location: "Castle Morne Rampart",
+    link: "https://eldenring.wiki.fextralife.com/Nomadic+Merchant+Castle+Morne+Rampart",
+    stock: [
+        [kukri, 60],
+        [arrow, 20],
+        [bolt, 40],
+    ],
+}
+
+const nomadicMerchant5 = {
+    name: "Nomadic Merchant 5",
+    location: "Liurnia of the Lakes",
+    link: "https://eldenring.wiki.fextralife.com/Nomadic+Merchant+Liurnia+of+the+Lakes",
+    stock: [
+        [arrow, 20],
+        [bolt, 40],
+    ],
+}
+
+const nomadicMerchant6 = {
+    name: "Nomadic Merchant 6",
+    location: "North Liurnia of the Lakes",
+    link: "https://eldenring.wiki.fextralife.com/Nomadic+Merchant+North+Liurnia+of+the+Lakes",
+    stock: [
+        [arrow, 20],
+        [bolt, 40],
+    ],
+}
+
+const nomadicMerchant7 = {
+    name: "Nomadic Merchant 7",
+    location: "West Altus Plateau",
+    link: "https://eldenring.wiki.fextralife.com/Nomadic+Merchant+West+Altus+Plateau",
+    stock: [
+        [arrow, 20],
+        [bolt, 40],
+        [lightningBolt, 160],
+    ],
+}
+
+const nomadicMerchant8 = {
+    name: "Nomadic Merchant 8",
+    location: "Mt. Gelmir",
+    link: "https://eldenring.wiki.fextralife.com/Nomadic+Merchant+Mt.+Gelmir",
+    stock: [
+        [arrow, 20],
+        [greatArrow, 300],
+        [bolt, 40],
+        [explosiveBolt, 160],
+        [ballistaBolt, 300],
+        [explosiveGreatBolt, 800],
+    ],
+}
+
+const nomadicMerchant9 = {
+    name: "Nomadic Merchant 9",
+    location: "Caelid Highway North",
+    link: "https://eldenring.wiki.fextralife.com/Wandering+Merchant+Caelid+Highway+North",
+    stock: [
+        [poisonboneDart, 120],
+        [poisonedStone, 150],
+        [poisonedStoneClump, 250],
+        [arrow, 20],
+        [fireArrow, 120],
+        [bolt, 40],
+    ],
+}
+
+const nomadicMerchant10 = {
+    name: "Nomadic Merchant 10",
+    location: "Caelid South",
+    link: "https://eldenring.wiki.fextralife.com/Nomadic+Merchant+Caelid+South",
+    stock: [
+        [arrow, 20],
+        [bolt, 40],
+        [flamingBolt, 120],
+    ],
+}
+
+const isolatedMerchant1 = {
+    name: "Isolated Merchant 1",
+    location: "Weeping Pininsula",
+    link: "https://eldenring.wiki.fextralife.com/Isolated+Merchant+Weeping+Peninsula",
+    stock: [
+        [arrow, 20],
+        [bolt, 40],
+    ],
+}
+
+const isolatedMerchant2 = {
+    name: "Isolated Merchant 2",
+    location: "Raya Lucaria",
+    link: "https://eldenring.wiki.fextralife.com/Isolated+Merchant+Raya+Lucaria",
+    stock: [
+        [meteorBolt, 120],
+    ],
+}
+
+const isolatedMerchant3 = {
+    name: "Isolated Merchant 3",
+    location: "Dragonbarrow",
+    link: "https://eldenring.wiki.fextralife.com/Isolated+Merchant+Dragonbarrow",
+    stock: [
+        [arrow, 20],
+        [serpentArrow, 120],
+        [bolt, 40],
+    ],
+}
+
+const hermitMerchant1 = {
+    name: "Hermit Merchant 1",
+    location: "Leyndell",
+    link: "https://eldenring.wiki.fextralife.com/Hermit+Merchant",
+    stock: [
+        [festeringBloodyfinger, 3000],
+        [arrow, 20],
+        [goldenArrow, 120],
+        [greatArrow, 300],
+        [bolt, 40],
+        [goldenBolt, 120],
+        [ballistaBolt, 300],
+    ],
+}
+
+const hermitMerchant2 = {
+    name: "Hermit Merchant 2",
+    location: "Mountaintops of the Giants East",
+    link: "https://eldenring.wiki.fextralife.com/Hermit+Merchant+Mountaintops+East",
+    stock: [
+        [arrow, 20],
+        [greatArrow, 300],
+        [bolt, 40],
+        [ballistaBolt, 300],
+        [lightningGreatbolt, 500],
+    ],
+}
+
+const hermitMerchant3 = {
+    name: "Hermit Merchant 3",
+    location: "Ainsel River",
+    link: "https://eldenring.wiki.fextralife.com/Hermit+Merchant+Ainsel+River",
+    stock: [],
+}
+
+const imprisonedMerchant = {
+    name: "Imprisoned Merchant",
+    location: "Mohgwyn Dynasty Mausoleum",
+    link: "https://eldenring.wiki.fextralife.com/Imprisoned+Merchant",
+    stock: [
+        [bloodrose, 800],
+        [dwellingArrow, 160],
+        [burredBolt, 200],
+    ],
+}
+
+const abandonedMerchant = {
+    name: "Abandoned Merchant",
+    location: "Siofra River",
+    link: "https://eldenring.wiki.fextralife.com/Abandoned+Merchant+Siofra+River",
+    stock: [
+        [arrow, 20],
+        [bolt, 40],
+    ],
+}
+
+const kaleMerchant = {
+    name: "Merchant Kalé",
+    location: "Limgrave: Church of Elleh",
+    link: "https://eldenring.wiki.fextralife.com/Merchant+Kal%C3%A9",
+    stock: [
+        [throwingDagger, 40],
+        [arrow, 20],
+        [bolt, 40],
+    ],
+}
+
+const patches = {
+    name: "Patches",
+    location: "Limgrave: Murkwater Cave",
+    link: "https://eldenring.wiki.fextralife.com/Patches",
+    stock: [
+        [glassShard, 100],
+    ],
+}
+
+const boggart = {
+    name: "Blackguard Big Boggart",
+    location: "Liurnia of the Lakes: Boilprawn Shack",
+    link: "https://eldenring.wiki.fextralife.com/Blackguard+Big+Boggart",
+    stock: [
+        [boiledPrawn, 600],
+        [boiledCrab, 600],
+    ],
+}
+
+const iji = {
+    name: "War Counselor Iji",
+    location: "Liurnia of the Lakes: Kingsrealm Ruins",
+    link: "https://eldenring.wiki.fextralife.com/War+Counselor+Iji",
+    stock: [
+        [somberStone1, 2000],
+        [somberStone2, 3000],
+    ],
+}
+
+const enia = {
+    name: "Enia",
+    location: "Roundtable Hold",
+    link: "https://eldenring.wiki.fextralife.com/Enia",
+    stock: [
+        [radahnSpear, 800],
+    ],
+}
+
 const nomadicGroup = {
     name: "Nomadic Merchants",
     list: [
         nomadicMerchant1,
-        nomadicMerchant1,
+        nomadicMerchant2,
+        nomadicMerchant3,
+        nomadicMerchant4,
+        nomadicMerchant5,
+        nomadicMerchant6,
+        nomadicMerchant7,
+        nomadicMerchant8,
+        nomadicMerchant9,
+        nomadicMerchant10,
+    ],
+}
+
+const isolatedGroup = {
+    name: "Isolated Merchants",
+    list: [
+        isolatedMerchant1,
+        isolatedMerchant2,
+        isolatedMerchant3,
+    ],
+}
+
+const hermitGroup = {
+    name: "Hermit Merchants",
+    list: [
+        hermitMerchant1,
+        hermitMerchant2,
+        hermitMerchant3,
+    ],
+}
+
+const specialGroup = {
+    name: "Special Merchants",
+    list: [
+        kaleMerchant,
+        abandonedMerchant,
+        imprisonedMerchant,
+    ],
+}
+
+const npcGroup = {
+    name: "NPC Merchants",
+    list: [
+        patches,
+        boggart,
+        enia,
+        iji,
     ],
 }
 
 const groups = [
     nomadicGroup,
+    isolatedGroup,
+    hermitGroup,
+    specialGroup,
+    npcGroup,
 ]
